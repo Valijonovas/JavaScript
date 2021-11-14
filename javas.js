18560bfdab5355512f3efd7d75985709da60f6a0
Valijonovas <sarvinozvalijonova01@gmail.com>
2021-11-14 12:34:51 +0500

    tz14 pet2

---
+++ javas.js
@@ -0,0 +1,89 @@
+// let ism = 'Sarvinoz';
+// let familiya = 'Valijonova';
+// let millat = 'ozbek';
+// let yosh = 15;
+// let ogil = false;
+// let qiz = true;
+
+// let sarvinoz = {
+//     name: 'Sarvinoz',
+//     Surname: 'Valijonova',
+//     Age: 15,
+//     School_number: 48,
+// }
+
+// console.log('name:', sarvinoz.name);
+// console.log('surname:', sarvinoz.Surname);
+// console.log('Age:',sarvinoz.Age)
+// console.log('School_number:', sarvinoz.School_number)
+
+// console.log('Ism:', ism);
+// console.log('familiya:', familiya);
+// console.log('millat:', millat);
+// console.log('yosh:', yosh);
+// console.log('ogil:', ogil);
+// console.log('qiz:', qiz);
+// console.log('name:', name);
+// console.log('Age:', age);
+// console.log('Surname:', Surname);
+// console.log('School number:',)
+
+
+// let num = '2';
+// console.log(num + 2);
+
+// let num2 = Number(num)
+// console.log(num2 + 2);
+
+let num = 2;
+console.log(num)
+
+let str = String(num)
+console.log(str + 2)
+
+// TZ-14
+//  деление
+console.log(99 / 3)
+console.log(2 / 4)
+console.log(144 / 0)
+console.log('4' / 2)
+console.log('1 2' / 5)
+console.log(23 / Infinity)
+
+// умножение
+console.log('22' * 2)
+console.log('one' * 2)
+console.log(0.5 * 2)
+console.log(.2 * 2)
+
+// сложение
+console.log('3' + 2)
+console.log(2 + +'3')
+
+console.log(12 + 2)
+
+// вычитание
+console.log('23' - '3')
+console.log('23' - 3)
+
+let one = ' 1';
+let otherOne = 1;
+
+let year = 2021;
+let age = 34;
+console.log("Я родился в ", year - age, " году");
+
+let one = " 1";
+let otherOne = 1;
+console.log(one == otherOne);
+
+console.log(one === otherOne);
+console.log(Boolean(144));
+console.log(Boolean(0));
+console.log(Boolean({}));
+console.log(Boolean({
+    name: "Вася"
+}));
+console.log(Boolean([]));
+console.log(Boolean([1, 2, 3]));
+let year = 2021;
