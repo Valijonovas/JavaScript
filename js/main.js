2d37736fca2d432b1deb796fe71a0a6c10aefb3f
Valijonovas <sarvinozvalijonova01@gmail.com>
2021-11-17 15:22:38 +0500

    tz15

--- js/main.js
+++ js/main.js
@@ -1,21 +1,14 @@
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: true,
-    dots: false,
-    center: true,
-    responsive: {
-        0: {
-            items: 1
-        },
-        500: {
-            items: 1
-        },
-        600: {
-            items: 2
-        },
-        1000: {
-            items: 3
-        }
-    }
-})
+// $('.owl-carousel').owlCarousel({
+//     loop: true,
+//     margin: 10,
+//     nav: true,
+//     dots: false,
+//     center: true,
+// })
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        nav: true,
+    });
+});
